5e8082ade9b18c67ba5716a5d120d5ce26aac30d
Timothy Clancy <tim@rockhopper.us>
2021-07-16 18:51:00 -0400

    Added natspec.

--- contracts/PermitControl.sol
+++ contracts/PermitControl.sol
@@ -75,9 +75,14 @@ abstract contract PermitControl is Context, Ownable {
   /**
     A modifier which allows only the super-administrative owner or addresses
     with a specified valid right to perform a call.
+
+    @param _circumstance The circumstance under which to check for the validity
+      of the specified `right`.
+    @param _right The right to validate for the calling address. It must be
+      non-expired and exist within the specified `_circumstance`.
   */
   modifier hasValidPermit(bytes32 _circumstance, bytes32 _right) {
-    require(_msgSender() == owner() || hasRightUntil(_msgSender(), _circumstance, _right) > 0,
+    require(_msgSender() == owner() || hasRightUntil(_msgSender(), _circumstance, _right) > block.timestamp,
       "PermitControl: sender does not have a valid permit");
     _;
   }
@@ -85,6 +90,12 @@ abstract contract PermitControl is Context, Ownable {
   /**
     Determine whether or not an address has some rights under the given
     circumstance, and if they do have the right, until when.
+
+    @param _address The address to check for the specified `_right`.
+    @param _circumstance The circumstance to check the specified `_right` for.
+    @param _right The right to check for validity.
+    @return The timestamp in seconds when the `_right` expires. If the timestamp is
+      zero, we can assume that the user never had the right.
   */
   function hasRightUntil(address _address, bytes32 _circumstance, bytes32 _right) public view returns (uint256) {
     return permissions[_address][_circumstance][_right];
@@ -94,6 +105,12 @@ abstract contract PermitControl is Context, Ownable {
     Set the permit to a specific address under some circumstances. A permit may
     only be set by the super-administrative contract owner or an address holding
     some delegated management permit.
+
+    @param _address The address to assign the specified `_right` to.
+    @param _circumstance The circumstance in which the `_right` is valid.
+    @param _right The specific right to assign.
+    @param _expirationTime The time when the `_right` expires for the provided
+      `_circumstance`.
   */
   function setPermit(address _address, bytes32 _circumstance, bytes32 _right, uint256 _expirationTime) external virtual hasValidPermit(UNIVERSAL, managerRight[_right]) {
     require(_right != ZERO_RIGHT,
@@ -105,6 +122,11 @@ abstract contract PermitControl is Context, Ownable {
   /**
     Set the `_managerRight` whose `UNIVERSAL` holders may freely manage the
     specified `_managedRight`.
+
+    @param _managedRight The right which is to have its manager set to
+      `_managerRight`.
+    @param _managerRight The right whose `UNIVERSAL` holders may manage
+      `_managedRight`.
   */
   function setManagerRight(bytes32 _managedRight, bytes32 _managerRight) external virtual hasValidPermit(UNIVERSAL, MANAGER) {
     require(_managedRight != ZERO_RIGHT,
