4f218ba41cee4ea858c626b431b3d6dfa7e63394
Timothy Clancy <tim@rockhopper.us>
2021-09-12 02:57:43 -0400

    Added Super721 interface.

---
+++ contracts/ISuper721.sol
@@ -0,0 +1,426 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.6;
+pragma experimental ABIEncoderV2;
+
+/**
+  @title An interface for the `Super721` ERC-721 item collection contract.
+  @author 0xthrpw
+  @author Tim Clancy
+
+  September 11th, 2021.
+*/
+interface ISuper721 {
+
+  /// The public identifier for the right to set this contract's metadata URI.
+  function SET_URI () external view returns (bytes32);
+
+  /// The public identifier for the right to set this contract's proxy registry.
+  function SET_PROXY_REGISTRY () external view returns (bytes32);
+
+  /// The public identifier for the right to configure item groups.
+  function CONFIGURE_GROUP () external view returns (bytes32);
+
+  /// The public identifier for the right to mint items.
+  function MINT () external view returns (bytes32);
+
+  /// The public identifier for the right to burn items.
+  function BURN () external view returns (bytes32);
+
+  /// The public identifier for the right to set item metadata.
+  function SET_METADATA () external view returns (bytes32);
+
+  /// The public identifier for the right to lock the metadata URI.
+  function LOCK_URI () external view returns (bytes32);
+
+  /// The public identifier for the right to lock an item's metadata.
+  function LOCK_ITEM_URI () external view returns (bytes32);
+
+  /// The public identifier for the right to disable item creation.
+  function LOCK_CREATION () external view returns (bytes32);
+
+  /// The public name of this contract.
+  function name () external view returns (string memory);
+
+  /// The public symbol of this contract.
+  function symbol () external view returns (string memory);
+
+  /**
+    An ERC-1155 URI for tracking item metadata, supporting {id} substitution.
+    For example: https://token-cdn-domain/{id}.json. See the ERC-1155 spec for
+    more details: https://eips.ethereum.org/EIPS/eip-1155#metadata.
+  */
+  function metadataUri () external view returns (string memory);
+
+  /// A proxy registry address for supporting automatic delegated approval.
+  function proxyRegistryAddress () external view returns (address);
+
+  /// A mapping from each token ID to per-address balances.
+  function balances (uint256, address) external view returns (uint256);
+
+  /// A mapping from each group ID to per-address balances.
+  function groupBalances (uint256, address) external view returns (uint256);
+
+  /// A mapping from each address to a collection-wide balance.
+  function totalBalances (address) external view returns (uint256);
+
+  /**
+    This enumeration lists the various supply types that each item group may
+    use. In general, the administrator of this collection or those permissioned
+    to do so may move from a more-permissive supply type to a less-permissive.
+    For example: an uncapped or flexible supply type may be converted to a
+    capped supply type. A capped supply type may not be uncapped later, however.
+
+    @param Capped There exists a fixed cap on the size of the item group. The
+      cap is set by `supplyData`.
+    @param Uncapped There is no cap on the size of the item group. The value of
+      `supplyData` cannot be set below the current circulating supply but is
+      otherwise ignored.
+    @param Flexible There is a cap which can be raised or lowered (down to
+      circulating supply) freely. The value of `supplyData` cannot be set below
+      the current circulating supply and determines the cap.
+  */
+  enum SupplyType {
+    Capped,
+    Uncapped,
+    Flexible
+  }
+
+  /**
+    This enumeration lists the various burn types that each item group may use.
+    These are static once chosen.
+
+    @param None The items in this group may not be burnt. The value of
+      `burnData` is ignored.
+    @param Burnable The items in this group may be burnt. The value of
+      `burnData` is the maximum that may be burnt.
+    @param Replenishable The items in this group, once burnt, may be reminted by
+      the owner. The value of `burnData` is ignored.
+  */
+  enum BurnType {
+    None,
+    Burnable,
+    Replenishable
+  }
+
+  /**
+    This struct is a source of mapping-free input to the `configureGroup`
+    function. It defines the settings for a particular item group.
+
+    @param name A name for the item group.
+    @param supplyType The supply type for this group of items.
+    @param supplyData An optional integer used by some `supplyType` values.
+    @param burnType The type of burning permitted by this item group.
+    @param burnData An optional integer used by some `burnType` values.
+  */
+  struct ItemGroupInput {
+    string name;
+    SupplyType supplyType;
+    uint256 supplyData;
+    BurnType burnType;
+    uint256 burnData;
+  }
+
+  /**
+    This struct defines the settings for a particular item group and is tracked
+    in storage.
+
+    @param initialized Whether or not this `ItemGroup` has been initialized.
+    @param name A name for the item group.
+    @param supplyType The supply type for this group of items.
+    @param supplyData An optional integer used by some `supplyType` values.
+    @param burnType The type of burning permitted by this item group.
+    @param burnData An optional integer used by some `burnType` values.
+    @param circulatingSupply The number of individual items within this group in
+      circulation.
+    @param mintCount The number of times items in this group have been minted.
+    @param burnCount The number of times items in this group have been burnt.
+  */
+  struct ItemGroup {
+    bool initialized;
+    string name;
+    SupplyType supplyType;
+    uint256 supplyData;
+    BurnType burnType;
+    uint256 burnData;
+    uint256 circulatingSupply;
+    uint256 mintCount;
+    uint256 burnCount;
+  }
+
+  /// A mapping of data for each item group.
+  function itemGroups (uint256) external view returns (ItemGroup memory);
+
+  /// A mapping of circulating supplies for each individual token.
+  function circulatingSupply (uint256) external view returns (uint256);
+
+  /// A mapping of the number of times each individual token has been minted.
+  function mintCount (uint256) external view returns (uint256);
+
+  /// A mapping of the number of times each individual token has been burnt.
+  function burnCount (uint256) external view returns (uint256);
+
+  /**
+    A mapping of token ID to a boolean representing whether the item's metadata
+    has been explicitly frozen via a call to `lockURI(string calldata _uri,
+    uint256 _id)`. Do note that it is possible for an item's mapping here to be
+    false while still having frozen metadata if the item collection as a whole
+    has had its `uriLocked` value set to true.
+  */
+  function metadataFrozen (uint256) external view returns (bool);
+
+  /**
+    A public mapping of optional on-chain metadata for each token ID. A token's
+    on-chain metadata is unable to be changed if the item's metadata URI has
+    been permanently fixed or if the collection's metadata URI as a whole has
+    been frozen.
+  */
+  function metadata (uint256) external view returns (string memory);
+
+  /// Whether or not the metadata URI has been locked to future changes.
+  function uriLocked () external view returns (bool);
+
+  /// Whether or not the item collection has been locked to all further minting.
+  function locked () external view returns (bool);
+
+  /**
+    Return a version number for this contract's interface.
+  */
+  function version () external view returns (uint256);
+
+  /**
+    Return the item collection's metadata URI. This implementation returns the
+    same URI for all tokens within the collection and relies on client-side
+    ID substitution per https://eips.ethereum.org/EIPS/eip-1155#metadata. Per
+    said specification, clients calling this function must replace the {id}
+    substring with the actual token ID in hex, not prefixed by 0x, and padded
+    to 64 characters in length.
+
+    @return The metadata URI string of the item with ID `_itemId`.
+  */
+  function uri (uint256) external view returns (string memory);
+
+  /**
+    Allow the item collection owner or an approved manager to update the
+    metadata URI of this collection. This implementation relies on a single URI
+    for all items within the collection, and as such does not emit the standard
+    URI event. Instead, we emit our own event to reflect changes in the URI.
+
+    @param _uri The new URI to update to.
+  */
+  function setURI (string memory _uri) external;
+
+  /**
+    Allow the item collection owner or an approved manager to update the proxy
+    registry address handling delegated approval.
+
+    @param _proxyRegistryAddress The address of the new proxy registry to
+      update to.
+  */
+  function setProxyRegistry (address _proxyRegistryAddress) external;
+
+
+  /**
+    Retrieve the balance of a held tokens for a particular address `_owner`.
+
+    @param _owner The owner to check for this token balance.
+    @return The amount of token `_id` owned by `_owner`.
+  */
+  function balanceOf (address _owner) external view returns (uint256);
+
+  /**
+    Retrieve in a single call the balances of some mulitple particular token
+    `_ids` held by corresponding `_owners`.
+
+    @param _owners The owners to check for token balances.
+    @param _ids The IDs of tokens to check for balances.
+    @return the amount of each token owned by each owner.
+  */
+  function balanceOfBatch (address[] memory _owners, uint256[] memory _ids) external view returns (uint256[] memory);
+
+  /**
+    TODO
+  */
+  function approve (address _to, uint256 _id) external;
+
+  /**
+    This function returns true if `_operator` is approved to transfer items
+    owned by `_owner`. This approval check features an override to explicitly
+    whitelist any addresses delegated in the proxy registry.
+
+    @param _owner The owner of items to check for transfer ability.
+    @param _operator The potential transferrer of `_owner`'s items.
+    @return Whether `_operator` may transfer items owned by `_owner`.
+  */
+  function isApprovedForAll (address _owner, address _operator) external view returns (bool);
+
+  /**
+    Enable or disable approval for a third party `_operator` address to manage
+    (transfer or burn) all of the caller's tokens.
+
+    @param _operator The address to grant management rights over all of the
+      caller's tokens.
+    @param _approved The status of the `_operator`'s approval for the caller.
+  */
+  function setApprovalForAll (address _operator, bool _approved) external;
+
+  /**
+    TODO
+  */
+  function ownerOf (uint256 _id) external view returns (address);
+
+  /**
+    TODO
+  */
+  /**
+    Retrieve the balance of a particular token `_id` for a particular address
+    `_owner`.
+
+    @param _owner The owner to check for this token balance.
+    @param _id The ID of the token to check for a balance.
+    @return The amount of token `_id` owned by `_owner`.
+  */
+  function balanceOfGroup (address _owner, uint256 _id) external view returns (uint256);
+
+  /**
+    Transfer on behalf of a caller or one of their authorized token managers
+    items from one address to another.
+
+    @param _from The address to transfer tokens from.
+    @param _to The address to transfer tokens to.
+    @param _id The specific token ID to transfer.
+  */
+  function safeTransferFrom (address _from, address _to, uint256 _id) external;
+
+  /**
+    Transfer on behalf of a caller or one of their authorized token managers
+    items from one address to another.
+
+    @param _from The address to transfer tokens from.
+    @param _to The address to transfer tokens to.
+    @param _id The specific token ID to transfer.
+    @param _data Additional call data to send with this transfer.
+  */
+  function safeTransferFrom (address _from, address _to, uint256 _id, bytes memory _data) external;
+
+  /**
+    Transfer on behalf of a caller or one of their authorized token managers
+    items from one address to another.
+
+    @param _from The address to transfer tokens from.
+    @param _to The address to transfer tokens to.
+    @param _id The specific token ID to transfer.
+    @param _amount The amount of the specific `_id` to transfer.
+    @param _data Additional call data to send with this transfer.
+  */
+  function safeTransferFrom (address _from, address _to, uint256 _id, uint256 _amount, bytes memory _data) external;
+
+  /**
+    Transfer on behalf of a caller or one of their authorized token managers
+    items from one address to another.
+
+    @param _from The address to transfer tokens from.
+    @param _to The address to transfer tokens to.
+    @param _ids The specific token IDs to transfer.
+    @param _amounts The amounts of the specific `_ids` to transfer.
+    @param _data Additional call data to send with this transfer.
+  */
+  function safeBatchTransferFrom (address _from, address _to, uint256[] memory _ids, uint256[] memory _amounts, bytes memory _data) external;
+
+  /**
+    TODO
+  */
+  function transferFrom (address _from, address _to, uint256 _id) external;
+
+  /**
+    Create a new NFT item group or configure an existing one. NFTs within a
+    group share a group ID in the upper 128-bits of their full item ID.
+    Within a group NFTs can be distinguished for the purposes of serializing
+    issue numbers.
+
+    @param _groupId The ID of the item group to create or configure.
+    @param _data The `ItemGroup` data input.
+  */
+  function configureGroup (uint256 _groupId, bytes calldata _data) external;
+
+  /**
+    Mint a batch of tokens into existence and send them to the `_recipient`
+    address. In order to mint an item, its item group must first have been
+    created. Minting an item must obey both the fungibility and size cap of its
+    group.
+
+    @param _recipient The address to receive all NFTs within the newly-minted
+      group.
+    @param _ids The item IDs for the new items to create.
+    @param _amounts The amount of each corresponding item ID to create.
+    @param _data Any associated data to use on items minted in this transaction.
+  */
+  function mintBatch (address _recipient, uint256[] memory _ids, uint256[] memory _amounts, bytes memory _data) external;
+
+  /**
+    This function allows an address to destroy multiple different items in a
+    single call.
+
+    @param _burner The address whose items are burning.
+    @param _ids The item IDs to burn.
+    @param _amounts The amounts of the corresponding item IDs to burn.
+  */
+  function burnBatch (address _burner, uint256[] memory _ids, uint256[] memory _amounts) external;
+
+  /**
+    TODO
+  */
+  function getApproved (uint256 _id) external view returns (address);
+
+  /**
+    TODO
+  */
+  function tokenOfOwnerByIndex (address _owner, uint256 _index) external view returns (uint256);
+
+  /**
+    TODO
+  */
+  function totalSupply () external view returns (uint256);
+
+  /**
+    TODO
+  */
+  function tokenByIndex (uint256 _index) external view returns (uint256);
+
+  /**
+    TODO
+  */
+  function tokenURI (uint256 _id) external view returns (string memory);
+
+  /**
+    Set the on-chain metadata attached to a specific token ID so long as the
+    collection as a whole or the token specifically has not had metadata
+    editing frozen.
+
+    @param _id The ID of the token to set the `_metadata` for.
+    @param _metadata The metadata string to store on-chain.
+  */
+  function setMetadata (uint256 _id, string memory _metadata) external;
+
+  /**
+    Allow the item collection owner or an associated manager to forever lock the
+    metadata URI on the entire collection to future changes.
+
+    @param _uri The value of the URI to lock for `_id`.
+  */
+  function lockURI(string calldata _uri) external;
+
+  /**
+    Allow the item collection owner or an associated manager to forever lock the
+    metadata URI on an item to future changes.
+
+    @param _uri The value of the URI to lock for `_id`.
+    @param _id The token ID to lock a metadata URI value into.
+  */
+  function lockItemGroupURI(string calldata _uri, uint256 _id) external;
+
+  /**
+    Allow the item collection owner or an associated manager to forever lock
+    this contract to further item minting.
+  */
+  function lock() external;
+}
