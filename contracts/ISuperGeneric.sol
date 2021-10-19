8ae474be7e74c8a2c1db29537e9480b000c05a7c
Qazawat Zirak <89194195+qzirak@users.noreply.github.com>
2021-10-19 12:22:18 +0700

    Added StakerV2 and basic tests (#31)
    * Added StakerV2 and basic tests
    
    * StakerV2 NFT, FT, SFT item staking with PermitControl
    
    * StakerV2 missing natspec fix
    
    * StakerV2 Separated claim function

---
+++ contracts/ISuperGeneric.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+  @title A generic interface for Super721 and Super1155 used in SuperStaking
+  @author Qazawat Zirak
+ */
+interface ISuperGeneric is IERC165 {
+    /** 
+     * @dev safeBatchTransferFrom is not included in Original Openzeppelin IERC721.
+    */
+    function safeBatchTransferFrom(
+        address _from, 
+        address _to,
+        uint256[] memory _ids, 
+        bytes memory _data
+    ) external;
+
+    function safeBatchTransferFrom(
+        address _from, 
+        address _to,
+        uint256[] memory _ids, 
+        uint256[] memory _amounts, 
+        bytes memory _data
+    ) external;
+}
