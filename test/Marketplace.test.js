6c2df2bedc3d55ee0f31cefc25fe2466f231b2e2
letsleroyu <eny@kode-t.com>
2021-10-29 17:25:42 +0700

    Update

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -117,7 +117,7 @@ describe("SuperFarm Marketplace", function(){
         expect(await erc721.balanceOf(alice.address)).to.be.eq("2")
     })
 
-    it("MarketPlace offer with decreasing price", async function() {
+    it("MarketPlace listing with decreasing price", async function() {
         let salt = 1;
         let abi = ["function transferFrom(address from,address to,uint256 tokenId)"]
         let iface = new ethers.utils.Interface(abi)
@@ -284,6 +284,60 @@ describe("SuperFarm Marketplace", function(){
             utils.replacementPatternBuy,
             0,
             0
+        )
+        let sellHash = await marketplace.hashOrder(orderSell)
+        let buyHash = await marketplace.hashOrder(orderBuy)
+        let signatureSell = await bob.signMessage(ethers.utils.arrayify(sellHash));
+        let signatureBuy = await alice.signMessage(ethers.utils.arrayify(buyHash));
+        let sigSell = ethers.utils.splitSignature(signatureSell);
+        let sigBuy = ethers.utils.splitSignature(signatureBuy);
+        let proxy = await registry.proxies(bob.address)
+        await erc721.connect(bob).approve(proxy, 1)
+
+        await marketplace.connect(alice).atomicMatch_(orderBuy, {v: sigBuy.v, r: sigBuy.r, s: sigBuy.s}, orderSell, {v: sigSell.v, r: sigSell.r, s: sigSell.s}, "0x0000000000000000000000000000000000000000000000000000000000000000", [], [], { value: ethers.utils.parseEther("12")})
+
+        expect(await erc721.balanceOf(alice.address)).to.be.eq("2")
+    });
+
+    it("Marketplace offer listing", async function() {
+        let salt = 1;
+        let abi = ["function transferFrom(address from,address to,uint256 tokenId)"]
+        let iface = new ethers.utils.Interface(abi)
+        let dataSell = iface.encodeFunctionData("transferFrom", [bob.address, utils.NULL_ADDRESS, 1]);
+        let time = await utils.getCurrentTime()
+        let orderSell = utils.makeOrder(
+            ethers.utils.parseEther("0.1"),
+            utils.NULL_ADDRESS,
+            exchangeToken.address,
+            time,
+            salt, 
+            protocolFeeRecipient.address,
+            marketplace.address,
+            bob.address,
+            utils.NULL_ADDRESS,
+            erc721.address,
+            dataSell,
+            utils.replacementPatternSell,
+            1,
+            3
+        )
+        salt++
+        let dataBuy = iface.encodeFunctionData("transferFrom", [utils.NULL_ADDRESS, alice.address, 1]);
+        let orderBuy = utils.makeOrder(
+            ethers.utils.parseEther("0.12"),
+            utils.NULL_ADDRESS,
+            exchangeToken.address,
+            time,
+            salt,
+            utils.NULL_ADDRESS,
+            marketplace.address,
+            alice.address,
+            utils.NULL_ADDRESS,
+            erc721.address,
+            dataBuy,
+            utils.replacementPatternBuy,
+            0,
+            3
         )
         let sellHash = await marketplace.hashOrder(orderSell)
         let buyHash = await marketplace.hashOrder(orderBuy)
