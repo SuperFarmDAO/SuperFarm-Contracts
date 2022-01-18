9ceb96852e70de094f9ec04c68f25ad869d179f5
Khlebnikov Rostislav <khlebnikovrostislav@gmail.com>
2022-01-18 20:10:31 +0700

    marketplace v2 initial

--- contracts/libraries/EIP712.sol
+++ contracts/libraries/EIP712.sol
@@ -1,33 +1,35 @@
 pragma solidity ^0.8.8;
 
 abstract contract EIP712 {
-
     struct EIP712Domain {
-        string  name;
-        string  version;
+        string name;
+        string version;
         uint256 chainId;
         address verifyingContract;
     }
 
-    bytes32 constant EIP712DOMAIN_TYPEHASH = keccak256(
-        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-    );
+    bytes32 constant EIP712DOMAIN_TYPEHASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        );
 
     bytes internal personalSignPrefix = "\x19Ethereum Signed Message:\n";
 
-    bytes32 immutable public DOMAIN_SEPARATOR;
-    
-    constructor(string memory name, string memory version){
-        uint chainId_;
-        assembly{
+    bytes32 public immutable DOMAIN_SEPARATOR;
+
+    constructor(string memory name, string memory version) {
+        uint256 chainId_;
+        assembly {
             chainId_ := chainid()
         }
-        DOMAIN_SEPARATOR = hash(EIP712Domain({
-            name              : name,
-            version           : version,
-            chainId           : chainId_,
-            verifyingContract : address(this)
-        }));
+        DOMAIN_SEPARATOR = hash(
+            EIP712Domain({
+                name: name,
+                version: version,
+                chainId: chainId_,
+                verifyingContract: address(this)
+            })
+        );
     }
 
     function hash(EIP712Domain memory eip712Domain)
@@ -35,19 +37,26 @@ abstract contract EIP712 {
         pure
         returns (bytes32)
     {
-        return keccak256(abi.encode(
-            EIP712DOMAIN_TYPEHASH,
-            keccak256(bytes(eip712Domain.name)),
-            keccak256(bytes(eip712Domain.version)),
-            eip712Domain.chainId,
-            eip712Domain.verifyingContract
-        ));
+        return
+            keccak256(
+                abi.encode(
+                    EIP712DOMAIN_TYPEHASH,
+                    keccak256(bytes(eip712Domain.name)),
+                    keccak256(bytes(eip712Domain.version)),
+                    eip712Domain.chainId,
+                    eip712Domain.verifyingContract
+                )
+            );
     }
 
     function parseSignature(bytes memory signature)
         internal
         pure
-        returns (uint8 v, bytes32 r, bytes32 s)
+        returns (
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        )
     {
         // ecrecover takes the signature parameters, and the only way to get them
         // currently is to use assembly.
@@ -57,6 +66,6 @@ abstract contract EIP712 {
             s := mload(add(signature, 0x40))
             v := byte(0, mload(add(signature, 0x60)))
         }
-        return(v,r,s);
+        return (v, r, s);
     }
 }
