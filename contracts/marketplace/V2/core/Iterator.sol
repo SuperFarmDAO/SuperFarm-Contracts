722b5a541529e72b137437cb8d0178fe225f2c3f
Rostislav Khlebnikov <khlebnikovrostislav@gmail.com>
2022-01-18 21:03:59 +0700

    eip712 support

--- contracts/marketplace/V2/core/Iterator.sol
+++ contracts/marketplace/V2/core/Iterator.sol
@@ -4,20 +4,20 @@ library IteratorLib{
     struct Iterator{
         uint256 type_i;
         uint256 target_i;
-        uint256 selector_i;
+        uint256 args_i;
     }
     //ITERATOR SHOULD BE BASED ON TYPE
-    function increment(Iterator memory iterator) internal pure returns(Iterator memory){
+    function increment(Iterator memory iterator, uint args_len) internal pure returns(Iterator memory){
         iterator.type_i += 4;
         iterator.target_i += 20;
-        iterator.selector_i += 4;
+        iterator.args_i += 32*args_len;
         return iterator;
     }
 
     function void(Iterator memory iterator) internal pure returns(Iterator memory){
         iterator.type_i = 0;
         iterator.target_i = 0;
-        iterator.selector_i = 0;
+        iterator.args_i = 0;
         return iterator;
     }
 }
