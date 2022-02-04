52d5f72d99a6bbcb8c61a3a83c325a6a6be6f6d9
Timothy Clancy <tim@rockhopper.us>
2022-02-04 06:57:50 -0500

    WIP EIP-712 claimable token.

---
+++ contracts/assets/erc20/ClaimableToken.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.8;
+
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+import "./Token.sol";
+
+/**
+  @title A version of the SuperFarm Token which may be claimed via signatures
+  that were produced off-chain. This token is intended to be issued as part of a
+  claimable airdrop.
+  @author Tim Clancy
+
+  This token contract was developed at a time when airdrops based on off-chain
+  activity like OpenSea trading volume were very popular.
+
+  February 4th, 2022.
+*/
+contract ClaimableToken is Token, EIP712 {
+
+  /// A constant hash of the mint operation's signature.
+  bytes32 constant public MINT_TYPEHASH = keccak256(
+    "mint(address _to,uint256 _amount)"
+  );
+
+  /// The address permitted to sign claim signatures.
+  address public immutable signer;
+
+  /// The total amount of this token that may be claimed across all claimants.
+  uint256 public immutable claimableCap;
+
+  /// The time when claiming this token begins.
+  uint256 public claimBegins;
+
+  /// The time when claiming this token ends.
+  uint256 public claimEnds;
+
+  /// A mapping for whether or not a given address has claimed their tokens.
+  mapping ( address => bool ) public claimed;
+
+  /// Track the amount of this token that has been claimed by claimants.
+  uint256 public claimedAmount = 0;
+
+  /**
+    An event emitted when a claimant claims tokens.
+
+    @param timestamp The timestamp of the claim.
+    @param claimant The caller who claimed tokens.
+    @param amount The amount of tokens claimed.
+  */
+  event Claimed (
+    uint256 timestamp,
+    address indexed claimant,
+    uint256 amount
+  );
+
+  /**
+    Construct a new ClaimableToken by providing it a name, ticker, supply cap,
+    permissioned claim signer, claimbale supply cap, and the times at which
+    claiming begins and ends.
+
+    @param _name The name of the new ClaimableToken.
+    @param _ticker The ticker symbol of the new ClaimableToken.
+    @param _cap The supply cap of the new ClaimableToken.
+    @param _signer The address permitted to sign claim signatures.
+    @param _claimableCap The cap on the total amount of token for claims.
+    @param _claimBegins The time at which claims for this token begin.
+    @param _claimEnds The time at which claims for this token end.
+  */
+  constructor (
+    string memory _name,
+    string memory _ticker,
+    uint256 _cap,
+    address _signer,
+    uint256 _claimableCap,
+    uint256 _claimBegins,
+    uint256 _claimEnds
+  ) Token(_name, _ticker, _cap) EIP712(_name, "1") {
+    signer = _signer;
+    claimableCap = _claimableCap;
+    claimBegins = _claimBegins;
+    claimEnds = _claimEnds;
+  }
+
+  /**
+    A private helper function to validate a signature supplied for token claims.
+    This function constructs a digest and verifies that the signature signer was
+    the authorized address we expect.
+
+    @param _claimant The claimant attempting to claim tokens.
+    @param _amount The amount of tokens the claimant is trying to claim.
+    @param _v The recovery byte of the signature.
+    @param _r Half of the ECDSA signature pair.
+    @param _s Half of the ECDSA signature pair.
+  */
+  function validClaim (
+    address _claimant,
+    uint256 _amount,
+    uint8 _v,
+    bytes32 _r,
+    bytes32 _s
+  ) private view returns (bool) {
+    bytes32 digest = ECDSA.toEthSignedMessageHash(
+      keccak256(
+        abi.encodePacked(
+          ECDSA.toTypedDataHash(
+            _domainSeparatorV4(),
+            keccak256(
+              abi.encode(
+                MINT_TYPEHASH,
+                _claimant,
+                _amount
+              )
+            )
+          )
+        )
+      )
+    );
+
+    // The claim is validated if it was signed by our authorized signer.
+    return ecrecover(digest, _v, _r, _s) == signer;
+  }
+
+  /**
+    Allow a caller to claim any of their available tokens if
+      1. the claiming period is active
+      2. the caller has not already claimed tokens
+      3. the claim would not exceed the claim cap
+      4. the claim is backed by a valid signature that has been pre-supplied
+         with a mint `_amount` by `signer`.
+
+    @param _amount The amount of tokens that the caller is trying to claim.
+    @param _v The recovery byte of the signature.
+    @param _r Half of the ECDSA signature pair.
+    @param _s Half of the ECDSA signature pair.
+  */
+  function claim (uint256 _amount, uint8 _v, bytes32 _r, bytes32 _s) external {
+    require(block.timestamp >= claimBegins && block.timestamp < claimEnds,
+      "0");
+    require(!claimed[_msgSender()],
+      "1");
+    require(claimedAmount + _amount <= claimableCap,
+      "2");
+    require(validClaim(_msgSender(), _amount, _v, _r, _s),
+      "3");
+
+    // Flag the caller as having claimed.
+    claimed[_msgSender()] = true;
+
+    // Mint tokens to the caller.
+    _mint(_msgSender(), _amount);
+
+    // Update our amount of claimed tokens.
+    claimedAmount += _amount;
+
+    // Emit an event.
+    emit Claimed(block.timestamp, _msgSender(), _amount);
+  }
+}
---
+++ test/ClaimableToken.test.js
@@ -0,0 +1,118 @@
+'use strict';
+
+// Imports.
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import 'chai/register-should';
+
+describe('ClaimableToken', function () {
+	let alice, bob, carol, dev;
+	let ClaimableToken;
+	before(async () => {
+		const signers = await ethers.getSigners();
+		const addresses = await Promise.all(signers.map(async signer => signer.getAddress()));
+		alice = { provider: signers[0].provider, signer: signers[0], address: addresses[0] };
+		bob = { provider: signers[1].provider, signer: signers[1], address: addresses[1] };
+		carol = { provider: signers[2].provider, signer: signers[2], address: addresses[2] };
+		dev = { provider: signers[3].provider, signer: signers[3], address: addresses[3] };
+
+    ClaimableToken = await ethers.getContractFactory('ClaimableToken');
+	});
+
+	// Deploy a fresh set of smart contracts for testing with.
+	let NAME = 'Testing Token';
+	let claimableToken;
+	beforeEach(async () => {
+		let now = Math.floor(new Date().getTime() / 1000);
+		await network.provider.send('evm_setNextBlockTimestamp', [ now ]);
+		await network.provider.send('evm_mine');
+		claimableToken = await ClaimableToken.connect(alice.signer).deploy(
+			NAME,
+			'TEST',
+			ethers.utils.parseEther('1000000000'),
+			dev.address,
+			ethers.utils.parseEther('1000000000'),
+			now - 600,
+			ethers.constants.MaxUint256
+		);
+		await claimableToken.deployed();
+	});
+
+	// Verify that claimants may claim tokens with valid signatures.
+	it('should allow claimants to claim', async () => {
+		const network = await ethers.getDefaultProvider().getNetwork();
+
+		// Retrieve the network chain ID.
+		let chainId = network.chainId;
+
+		// Construct the domain separator for this contract.
+		let domainSeparator = ethers.utils.keccak256(
+			ethers.utils.defaultAbiCoder.encode(
+				[ 'bytes32', 'bytes32', 'bytes32', 'uint256', 'address' ],
+				[
+					ethers.utils.keccak256(ethers.utils.toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')),
+					ethers.utils.keccak256(ethers.utils.toUtf8Bytes(NAME)),
+					ethers.utils.keccak256(ethers.utils.toUtf8Bytes('1')),
+					chainId,
+					claimableToken.address
+				]
+			)
+		);
+
+		// Construct the mint typehash.
+		let MINT_TYPEHASH = ethers.utils.keccak256(
+			ethers.utils.toUtf8Bytes('mint(address _to,uint256 _amount)')
+		);
+
+		// Construct a digest to give Bob tokens.
+		let digest = ethers.utils.keccak256(
+			ethers.utils.solidityPack(
+				[ 'bytes1', 'bytes1', 'bytes32', 'bytes32' ],
+				[
+					'0x19',
+					'0x01',
+					domainSeparator,
+					ethers.utils.keccak256(
+						ethers.utils.defaultAbiCoder.encode(
+							[ 'bytes32', 'address', 'uint256' ],
+							[ MINT_TYPEHASH, bob.address, ethers.utils.parseEther('1000') ]
+						)
+					)
+				]
+			)
+		);
+		let signedDigest = await dev.signer.signMessage(digest);
+console.log('manual', signedDigest)
+
+		// Our signer can now sign the digest to produce an executable signature.
+		let signature = await dev.signer._signTypedData(
+			{
+				name: NAME,
+				version: '1',
+				chainId: chainId,
+				verifyingContract: claimableToken.address
+			},
+			{
+				mint: [
+					{ name: '_to', type: 'address' },
+					{ name: '_amount', type: 'uint256' }
+				]
+			},
+			{
+				'_to': bob.address,
+				'_amount': ethers.utils.parseEther('1000')
+			}
+		);
+		let { v, r, s } = ethers.utils.splitSignature(signature);
+console.log('ethers', signature)
+
+		// Bob should be able to execute this signature.
+		// I can't get this working with either signature. :C
+		let bobBalance = await claimableToken.balanceOf(bob.address);
+		bobBalance.should.be.equal(0);
+		await claimableToken.connect(bob.signer).claim(
+			ethers.utils.parseEther('1000'), v, r, s);
+		bobBalance = await claimableToken.balanceOf(bob.address);
+		bobBalance.should.be.equal(ethers.utils.parseEther('1000'));
+	});
+});
