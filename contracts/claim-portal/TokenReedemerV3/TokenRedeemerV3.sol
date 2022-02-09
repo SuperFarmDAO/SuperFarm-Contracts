53fb22237d45a9c45c017522820b8f75c5b0e615
letsleroyu <eny@kode-t.com>
2022-02-09 15:19:49 +0700

    Updated contract, rewrited logic

--- contracts/claim-portal/TokenReedemerV3/TokenRedeemerV3.sol
+++ contracts/claim-portal/TokenReedemerV3/TokenRedeemerV3.sol
@@ -25,9 +25,10 @@ contract TokenRedeemerV3 is Ownable, ReentrancyGuard {
   ISuper721 public super721;
 
   /// The address being used as a custom burn address.
-  address public burnAddress;
+  address immutable public burnAddress;
 
-  bool public customBurn;
+  bytes4 private constant INTERFACE_ERC721 = 0x80ac58cd;
+
 
   /**
   */
@@ -35,33 +36,40 @@ contract TokenRedeemerV3 is Ownable, ReentrancyGuard {
     uint256 groupIdOut;
     uint256 amountOut;
     bool burnOnRedemption;
-    // address[] requirements;
-    // ReedeemNftType nftType;
+    bool customBurn;
+    address tokenOut;
     Requirement[] requirements;
+    mapping(address => bool) redeemed;
   }
 
   struct Requirement {
       address collection;
       uint256[] tokenId;
+      uint256[] amounts;
   }
 
-//   enum ReedeemNftType {
-//       ERC721,
-//       ERC1155
-//   }
+  struct RedemptionConfigStructCreation {
+    uint256 groupIdOut;
+    uint256 amountOut;
+    bool burnOnRedemption;
+    bool customBurn;
+    address tokenOut;
+    Requirement[] requirements;
+  }
 
   // groupIdIn => collection out => config
-  mapping (uint256 => mapping(address => RedemptionConfig)) public redemptionConfigs;
+  // mapping (uint256 => mapping(address => RedemptionConfig)) public redemptionConfigs;
+  mapping(uint256 => RedemptionConfig) public redemptionConfigs;
 
   // collection out => round in => address of redeemer
-  mapping (address => mapping (uint256 => address)) public redeemer;
+  // mapping (address => mapping (uint256 => address)) public redeemer;
 
   /**
   */
-  event TokenRedemption(address indexed user, uint256 indexed groupId, address indexed contractOut, uint256[] tokensOut);
+  event TokenRedemption(address indexed user, uint256 configId, uint256 timestamp);
 
-  /**
-  */
+  // /**
+  // */
   event ConfigUpdate(uint256 groupIdIn, uint256 groupIdOut, address tokenOut, uint256 amountOut, bool burnOnRedemption);
 
   /**
@@ -69,15 +77,34 @@ contract TokenRedeemerV3 is Ownable, ReentrancyGuard {
     address as `_burnTarget` and enable use of a custom burn address by setting
     `_customBurn`.
 
-    @param _burnTarget The address that will be used for burning tokens.
-    @param _customBurn Whether or not a custom burn address is used.
+    @param _burnAddress The address that will be used for burning tokens.
   */
-  constructor(address _burnTarget, bool _customBurn) {
-    customBurn = _customBurn;
-    if (customBurn) {
-      require(_burnTarget != address(0), "TokenRedeemer::constructor: Custom burn address cannot be 0 address");
-      burnAddress = _burnTarget;
-    }
+  constructor(address _burnAddress) {
+    burnAddress = _burnAddress;
+  }
+
+  function setRedemptionConfig(RedemptionConfigStructCreation calldata _data, uint256 _configId) internal {
+    require(_data.groupIdOut != 0, "TokenRedeemer::setRedemptionConfig: group id cannot be zero");
+    require(_data.amountOut != 0, "TokenRedeemer::setRedemptionConfig: amount out cannot be zero");
+    require(_data.tokenOut != address(0), "TokenRedeemer::setRedemptionConfig: token out cannot be zero address");
+    require(_data.requirements.length > 0, "TokenRedeemer::setRedemptionConfig: must specify requirements");
+
+    for(uint i = 0; i < _data.requirements.length; ++i) {
+        require(_data.requirements[i].collection != address(0), "TokenRedeemer::redeem: required token cannot be zero");
+        require(_data.requirements[i].tokenId.length > 0);
+        require(_data.requirements[i].amounts.length == _data.requirements[i].tokenId.length);
+      }
+
+    redemptionConfigs[_configId].groupIdOut = _data.groupIdOut;
+    redemptionConfigs[_configId].amountOut = _data.amountOut;
+    redemptionConfigs[_configId].burnOnRedemption = _data.burnOnRedemption;
+    redemptionConfigs[_configId].customBurn = _data.customBurn;
+    redemptionConfigs[_configId].requirements = _data.requirements;
+    redemptionConfigs[_configId].tokenOut = _data.tokenOut;
+
+
+
+    // emit ConfigUpdate(_groupIdIn, _data.groupIdOut, _tokenOut, _data.amountOut, _data.burnOnRedemption);
   }
 
   /**
@@ -85,57 +112,47 @@ contract TokenRedeemerV3 is Ownable, ReentrancyGuard {
     tokens group id, determines the appropriate exchange token and amount, if
     necessary burns the deposited token and mints the receipt token(s)
 
-    @param _tokenIds The array of 721 token ids
-    @param _tokenOut The address of the token being received
-    @param _groupIdIn The round or group id of the token being received
+    @param _configId The address of the token being received
   */
-  function redeem(uint256[] calldata _tokenIds, address _tokenOut, uint256 _groupIdIn) public nonReentrant {
-    RedemptionConfig memory config = redemptionConfigs[_groupIdIn][_tokenOut];
-    uint256 redemptionAmount = config.amountOut;
-    uint256 groupIdOut = config.groupIdOut;
+  function redeem(uint256 _configId) public nonReentrant {
 
-    uint256 mintCount = ISuper721(_tokenOut).groupMintCount(groupIdOut);
-    require(checkBalances(config.requirements), "TokenRedeemer::redeem: msg sender is not token owner");
 
-    {
-      require(_tokenIds.length == config.requirements.length, "TokenRedeemer::redeem: length mismatch");
+    RedemptionConfig storage config = redemptionConfigs[_configId];
+    require(!config.redeemed[msg.sender]);
+    Requirement[] memory req = config.requirements;
 
-      for(uint i = 0; i < _tokenIds.length; ++i){
-        require(redeemer[_tokenOut][_groupIdIn] == address(0), "TokenRedeemer::redeem: token has already been redeemed for this group" );
+    require(checkBalances(req), "TokenRedeemer::redeem: msg sender is not token owner");
+    
+    for (uint i = 0; i < req.length; i++) {
+      uint256[] memory tokenToBurnIds = req[i].tokenId;
+      uint256[] memory tokenToBurnAmounts = req[i].amounts;
+      address tokenToBurn = req[i].collection;
 
-        require(groupIdOut != uint256(0), "TokenRedeemer::redeem: invalid group id from token");
-        require(redemptionAmount != uint256(0), "TokenRedeemer::redeem: invalid redemption amount");
-        require(config.requirements[i].collection != address(0), "TokenRedeemer::redeem: required token cannot be zero");
-
-        // uint256 balanceOfSender = ISuperGeneric(config.requirements[i].collection).balanceOf(msg.sender, _tokenIds[i]);
-        // require(balanceOfSender != 0, "TokenRedeemer::redeem: msg sender is not token owner");
-
+      if (config.burnOnRedemption) {
+        if (config.customBurn) {
+          genericTransfer(msg.sender, burnAddress, tokenToBurn, tokenToBurnIds, tokenToBurnAmounts);
+        } else {
+          genericBurn(tokenToBurn, tokenToBurnIds, tokenToBurnAmounts);
+        }
       }
 
-      redeemer[_tokenOut][_groupIdIn] = msg.sender;
     }
 
+    uint256 redemptionAmount = config.amountOut;
+    uint256 mintCount = ISuper721(config.tokenOut).groupMintCount(config.groupIdOut);
     uint256[] memory ids = new uint256[](redemptionAmount);
 
-
     for(uint256 i = 0; i < redemptionAmount; i++) {
-      ids[i] = mintCount + i + 1;
+        ids[i] = mintCount + i + 1;
+        // amounts[i] = uint256(1);
     }
+    config.redeemed[msg.sender] = true;
+    ISuper721(config.tokenOut).mintBatch(msg.sender, ids, "");
 
-    // if (config.burnOnRedemption) {
-    //   if (customBurn) {
-    //     super1155.safeTransferFrom(msg.sender, burnAddress, _tokenId, 1, "");
-    //   } else {
-    //     super1155.burnBatch(msg.sender, _asSingletonArray(_tokenId), _asSingletonArray(1));
-    //   }
-    // }
-
-    ISuper721(_tokenOut).mintBatch(msg.sender, ids, "");
-
-    emit TokenRedemption(msg.sender, _groupIdIn, _tokenOut, ids);
+    // emit TokenRedemption(msg.sender, _groupIdIn, _tokenOut, ids);
   }
 
-  function checkBalances(Requirement[] memory requirements) internal view returns (bool) {
+  function checkBalances(Requirement[] memory requirements) private view returns (bool) {
       for (uint256 i = 0; i < requirements.length; i++) {
            for (uint256 j = 0; j < requirements[i].tokenId.length; j++) {
                 uint256 balanceOfSender = ISuperGeneric(requirements[i].collection).balanceOf(msg.sender, requirements[i].tokenId[j]);
@@ -145,55 +162,30 @@ contract TokenRedeemerV3 is Ownable, ReentrancyGuard {
       return true;
   }
 
-  /**
-    Redeem a specific set of tokens `_tokenIds` for a set of token `_tokenOut`
-    from group `_groupIdOut`
-
-    @param _tokenIds An array of bitpacked 1155 token ids
-    @param _tokenOuts The address of the token to receive
-    @param _groupIdsIn The group ids of the token to redeem
-  */
-  function redeemMult(uint256[][] calldata _tokenIds, address[] calldata _tokenOuts, uint256[] calldata _groupIdsIn) external {
-    for(uint256 n = 0; n < _tokenIds.length; n++){
-      redeem(_tokenIds[n], _tokenOuts[n], _groupIdsIn[n]);
+  
+  function genericBurn(address _assetAddress, uint256[] memory _ids, uint256[] memory _amounts) private returns (bool) {
+    bool isErc721 = ISuperGeneric(_assetAddress).supportsInterface(INTERFACE_ERC721) ? true : false;
+    if (!isErc721) {
+       ISuperGeneric(_assetAddress).burnBatch(address(this), _ids, _amounts);
+       return true;
+    } else if (isErc721) {
+      ISuperGeneric(_assetAddress).burnBatch(address(this), _ids);
+      return true;
     }
+    return false;
   }
 
-  /**
-    Configure redemption amounts for each group.  ONE token of _groupIdin results
-    in _amountOut number of _groupIdOut tokens
 
-    @param _groupIdIn The group ID of the token being redeemed
-    @param _groupIdIn The group ID of the token being received
-    @param _data The redemption config data input.
-  */
-
-  function setRedemptionConfig(uint256 _groupIdIn, address _tokenOut, RedemptionConfig calldata _data) external onlyOwner {
-    require(_data.groupIdOut != 0, "TokenRedeemer::setRedemptionConfig: group id cannot be zero");
-    require(_groupIdIn != 0, "TokenRedeemer::setRedemptionConfig: group id cannot be zero");
-
-    require(_tokenOut != address(0), "TokenRedeemer::setRedemptionConfig: token out cannot be zero address");
-    require(_data.requirements.length > 0, "TokenRedeemer::setRedemptionConfig: must specify requirements");
-
-    redemptionConfigs[_groupIdIn][_tokenOut] = RedemptionConfig({
-      groupIdOut: _data.groupIdOut,
-      amountOut: _data.amountOut,
-      burnOnRedemption: _data.burnOnRedemption,
-      requirements: _data.requirements
-    });
-
-    emit ConfigUpdate(_groupIdIn, _data.groupIdOut, _tokenOut, _data.amountOut, _data.burnOnRedemption);
+  function genericTransfer(address _from, address _to, address _assetAddress, uint256[] memory _ids, uint256[] memory _amounts) private returns (bool) {
+    bool isErc721 = ISuperGeneric(_assetAddress).supportsInterface(INTERFACE_ERC721) ? true : false;
+    if (!isErc721) {
+       ISuperGeneric(_assetAddress).safeBatchTransferFrom(_from, _to, _ids, _amounts, "");
+       return true;
+    } else if (isErc721) {
+      ISuperGeneric(_assetAddress).safeBatchTransferFrom(_from, _to, _ids, "");
+      return true;
+    }
+    return false;
   }
 
-  /**
-    This private helper function converts a number into a single-element array.
-
-    @param _element The element to convert to an array.
-    @return The array containing the single `_element`.
-  */
-  // function _asSingletonArray(uint256 _element) private pure returns (uint256[] memory) {
-  //   uint256[] memory array = new uint256[](1);
-  //   array[0] = _element;
-  //   return array;
-  // }
 }
